06666c3dc5bb528ea35e05321471824f21b44fac
Yassine197 <42244943+Yassine197@users.noreply.github.com>
2019-01-03 21:58:15 +0100

    Add files via upload

---
+++ Assets/Hache.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hache: MonoBehaviour {
+    public float min = 2f;
+    public float max = 3f;
+    public float speed = 9999999999999999999999999999999f;
+
+    // Use this for initialization
+    void Start () {
+        min = transform.position.x;
+        max = transform.position.x + 3;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        float rotate = speed * Time.deltaTime;
+        float currentRotation = gameObject.transform.rotation.z;
+        Quaternion rotation = Quaternion.Euler(0, 0, currentRotation + rotate);
+        gameObject.transform.rotation = rotation;
+        transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y, transform.position.z);
+
+    }
+}
